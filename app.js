7a46fa21ab9bb83047b45e8d5e12d51026e3fda7
przemskydev <przemekgithub@o2.pl>
2020-08-17 19:10:18 +0200

    adding put method

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const Joi = require('@hapi/joi');
 
-
 const express = require('express');
 const app = express();
 
 app.use(express.json())
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required()
+  })
+
+  return schema.validate(course);
+}
 
 const courses = [
   { id: 1, name: 'JavaScript' },
@@ -21,15 +28,10 @@ app.get('/api/courses', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body)
 
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(30).required()
-  })
-
-  const result = schema.validate(req.body);
-
-  if (result.error){
-    res.status(400).send(result.error.details[0].message);
+  if (error){
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -40,6 +42,28 @@ app.post('/api/courses', (req, res) => {
 
   courses.push(course)
   res.send(course)
+})
+
+app.put('/api/courses/:id', (req,res)=>{
+  const course = courses.find(course => course.id === parseInt(req.params.id))
+  if (!course) res.status(404).send('The course with the given ID was not found')
+
+  // const schema = Joi.object({
+  //   name: Joi.string().min(3).max(30).required()
+  // })
+
+  // const result = schema.validate(req.body);
+
+  const { error } = validateCourse(req.body)
+
+  if (error){
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course)
+
 })
 
 app.get('/api/courses/:id', (req, res) => {
