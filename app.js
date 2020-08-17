499d7c05dce9b60db2fb3a7d18cc93ab48e5947b
przemskydev <przemekgithub@o2.pl>
2020-08-17 19:29:47 +0200

    adding delete method

--- app.js
+++ app.js
@@ -30,10 +30,7 @@ app.get('/api/courses', (req, res) => {
 app.post('/api/courses', (req, res) => {
   const { error } = validateCourse(req.body)
 
-  if (error){
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -46,29 +43,30 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req,res)=>{
   const course = courses.find(course => course.id === parseInt(req.params.id))
-  if (!course) res.status(404).send('The course with the given ID was not found')
-
-  // const schema = Joi.object({
-  //   name: Joi.string().min(3).max(30).required()
-  // })
-
-  // const result = schema.validate(req.body);
+  if (!course) return res.status(404).send('The course with the given ID was not found')
 
   const { error } = validateCourse(req.body)
 
-  if (error){
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   course.name = req.body.name;
   res.send(course)
 
 })
 
+app.delete('/api/courses/:id', (req,res)=>{
+  const course = courses.find(course => course.id === parseInt(req.params.id))
+  if (!course) return res.status(404).send('The course with the given ID was not found')
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course)
+})
+
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(course => course.id === parseInt(req.params.id))
-  if (!course) res.status(404).send('The course with the given ID was not found')
+  if (!course) return res.status(404).send('The course with the given ID was not found')
   res.send(course)
 })
 
