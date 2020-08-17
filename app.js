4ed5662fd4aec418aa94f7b4b7b6a9e33ce69793
przemskydev <przemekgithub@o2.pl>
2020-08-17 18:50:18 +0200

    add joi input validate & post method

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
+const Joi = require('@hapi/joi');
+
+
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
+app.use(express.json())
+
+const courses = [
+  { id: 1, name: 'JavaScript' },
+  { id: 2, name: 'React' },
+  { id: 3, name: 'NodeJS' }
+]
+
+app.get('/', (req, res) => {
   res.send('Hello Node-World!')
 });
 
-app.get('/api/courses', (req, res)=>{
-  res.send([1,2,3,4,5]);
+app.get('/api/courses', (req, res) => {
+  res.send(courses);
 })
 
-app.listen(3000, ()=> console.log('Listening on 3000...'))
+app.post('/api/courses', (req, res) => {
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required()
+  })
+
+  const result = schema.validate(req.body);
+
+  if (result.error){
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+
+  courses.push(course)
+  res.send(course)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(course => course.id === parseInt(req.params.id))
+  if (!course) res.status(404).send('The course with the given ID was not found')
+  res.send(course)
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Listening on ${port}...`))
