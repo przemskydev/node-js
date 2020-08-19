058b3d7af9b5a94a8486ebf36cd116c03cb2a15a
przemskydev <przemekgithub@o2.pl>
2020-08-19 12:55:19 +0200

    connect MongoDB with node and using Robo3T as GUI

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-const http = require('http');
-const port = 3000;
-const handler = (request, response) => {
-  console.log('New user!')
-  response.end('Hello node!!')
-}
 
-const server = http.createServer(handler);
 
-server.listen(port, (err) => {
-  if(err){
-    return console.log('Some error')
-  }
+const { MongoClient, ObjectID } = require('mongodb');
 
-  console.log('Server is running...')
-});
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'mongo-test';
+
+MongoClient.connect(url, {}, (error, client) => {
+  if (error) console.log(`Some error check connection...`);
+
+  const db = client.db(dbName);
+  const id = new ObjectID()
+  // console.log(id.toHexString())
+  db.collection('users').insertOne({
+    _id: id,
+    name: 'Dan',
+    age: 23
+  }, (error, result)=>{
+    if(error) console.log(`Adding user error.`, error);
+
+    console.log(result.ops)
+  })
+
+  // db.collection('users').find({
+  //   age: { $gt: 35}
+  // }).toArray((error, result)=>{
+  //   if(error) console.log(`Can not find user`)
+
+  //   console.log(result)
+  // })
+
+
+  // db.collection('users').updateOne({
+  //   age: 143
+  // }, {
+  //   $set: {
+  //     age: 93
+  //   }
+  // })
+
+  // db.collection('users').deleteOne({
+  //   age: 32
+  // }, (error, result)=>console.log(result))
+
+  db.collection('users').find({}).toArray((error, result) => console.log(result))
+
+  console.log(`Database connection success.`);
+})
