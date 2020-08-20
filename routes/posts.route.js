c69751d8aad0602a787bb6efa3391c836964e386
przemskydev <przemekgithub@o2.pl>
2020-08-20 14:30:12 +0200

    searching post by id

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -2,17 +2,36 @@ const { Router } = require('express');
 const router = Router();
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => res.send('Posts site with router'));
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts)
+  } catch (error) {
+    res.send({ message })
+  }
+});
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
     desc: req.body.desc
   })
 
-  post.save()
-    .then(data => res.json(data))
-    .catch(error => console.log(error))
+  try {
+    const savePost = await post.save()
+    res.json(savePost)
+  } catch (error) {
+    res.send({ message })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post)
+  } catch (error) {
+    res.send({ message })
+  }
 })
 
 
