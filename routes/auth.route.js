3cdb826972c964c0e932114828f0d98261432af4
przemskydev <przemekgithub@o2.pl>
2020-08-20 23:27:45 +0200

    before setting validation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,13 +2,19 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 
-router.post('/register', (req, res)=>{
+router.post('/register',async (req, res)=>{
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
-
   })
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser)
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
 })
 
 
