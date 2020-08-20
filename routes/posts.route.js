a18c858a5cd41ed89cb2d0e5fd18e29510f409da
przemskydev <przemekgithub@o2.pl>
2020-08-20 14:43:41 +0200

    deleting and updating posts

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     const posts = await Post.find();
     res.json(posts)
   } catch (error) {
-    res.send({ message })
+    res.send(error)
   }
 });
 
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
     const savePost = await post.save()
     res.json(savePost)
   } catch (error) {
-    res.send({ message })
+    res.send(error)
   }
 })
 
@@ -30,7 +30,29 @@ router.get('/:id', async (req, res) => {
     const post = await Post.findById(req.params.id);
     res.json(post)
   } catch (error) {
-    res.send({ message })
+    res.send(error)
+  }
+})
+
+router.delete('/:id', async (req, res)=>{
+  try {
+    const removed = await Post.remove({_id: req.params.id})
+    res.json(removed)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.patch('/:id', async (req, res)=>{
+  try {
+    const updated = await Post.updateOne(
+      {_id: req.params.id},
+      { $set: {
+        title: req.body.title
+      }})
+      res.json(updated)
+  } catch (error) {
+    res.send(error)
   }
 })
 
