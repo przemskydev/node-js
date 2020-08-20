741ecf96871094fc3875e9726c0cc5b19606b33f
przemskydev <przemekgithub@o2.pl>
2020-08-20 12:47:36 +0200

    db mongodb connection working

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 const app = express();
+const DB_URI = process.env.DB_CONNECTION;
 
 //Routes
-app.get('/',(req, res)=>res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/posts', (req, res) => res.send('Posts site'))
+
+
+//connect to db
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+  if (!error) console.log(`Connection successful...`)
+})
 
 app.listen(port)
