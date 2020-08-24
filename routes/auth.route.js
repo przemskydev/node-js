3ee1c93507b3237350cf609842254949755c7893
przemskydev <przemekgithub@o2.pl>
2020-08-24 14:29:28 +0200

    if statement checking existing emails

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,9 +7,14 @@ router.post('/register', async (req, res) => {
   //validate before make a user
   const { error } = registerValidation(req.body)
 
-
   if(error) return res.status(400).send(error.details[0].message)
   
+  //checking if the user exist
+  const existEmail = await User.findOne({ email: req.body.email})
+
+  if(existEmail) return res.status(400).send(`Email alredy exist!`)
+
+  //new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
